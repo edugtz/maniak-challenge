30e543d646de423e3b5ece0c149e675a7a2c0938
edugtz <eduardo.gutierrez.2325@gmail.com>
2019-05-30 07:12:29 -0500

    Updating HeaderLinks to a presentational component with hooks

--- src/components/HeaderLinks/HeaderLinks.jsx
+++ src/components/HeaderLinks/HeaderLinks.jsx
@@ -1,71 +1,44 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 import { getAppData } from '../../api/api';
 import './HeaderLinks.scss';
 
-class HeaderLinks extends Component {
-    constructor(props) {
-        super(props);
+const HeaderLinks = () => {
+    const [menuItems, setMenuItems] = useState([]);
 
-        this.state = {
-            menuItems: []
-        };
-    };
-
-    componentDidMount() {
+    useEffect(() => {
         getAppData()
             .then(response => {
-                this.setState(() => {
-                    return {
-                        menuItems: response.data.menu.items
-                    };
-                });
+                setMenuItems(response.data.menu.items);
             })
             .catch(err => {
                 console.log(err);
             });
-    };
-
-    render() {
-        const { menuItems } = this.state;
-        return(
-            <nav className="nav-container">
-                <ul className="bellotero-nav" role="navigation">
-                    {menuItems.length !== 0 &&
-                        menuItems.map((item, index) => {
-                            return (
-                                <li className="bellotero-nav__item">
-                                    <NavLink
-                                        activeClassName="active-item" 
-                                        key={`${index}-${item.text}`} 
-                                        to={`/${item.route}`} >{item.text}
-                                    </NavLink>
-                                </li>
-                            );
-                        })
-                    }
-                    {/* <li className="bellotero-nav__item">
-                        <a href="#">Home</a>
-                    </li>
-                    <li className="bellotero-nav__item">
-                        <a href="#">Solutions</a>
-                    </li>
-                    <li className="bellotero-nav__item">
-                        <a href="#">Stories</a>
-                    </li>
-                    <li className="bellotero-nav__item">
-                        <a href="#">Partners</a>
-                    </li>
-                    <li className="bellotero-nav__item">
-                        <a href="#">About</a>
-                    </li>
-                    <li className="bellotero-nav__item">
-                        <a href="#">Blog</a>
-                    </li> */}
-                </ul>
-            </nav>
-        );
-    }
-};
+        
+    }, []);
+    
+    return (
+        <nav className="nav-container">
+            <ul className="bellotero-nav" role="navigation">
+                {menuItems.length !== 0 &&
+                    menuItems.map((item, index) => {
+                        return (
+                            <li key={`${index}-${item.text}`} 
+                                className="bellotero-nav__item">
+                                <NavLink
+                                    activeClassName="active-item" 
+                                    key={`${index}-${item.route}`} 
+                                    to={`/${item.route}`} >{item.text}
+                                </NavLink>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </nav>
+    );
+}
 
 export default withRouter(HeaderLinks);
