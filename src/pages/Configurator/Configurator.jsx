5bc6fbdb05728d6e8f66bc44f2cd6c137a77c8d8
edugtz <eduardo.gutierrez.2325@gmail.com>
2019-06-02 12:26:14 -0500

    Connecting Configurator page to redux store

--- src/pages/Configurator/Configurator.jsx
+++ src/pages/Configurator/Configurator.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import './Configurator.scss';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getConfiguratorData } from '../../actions/configuratorActions';
 
 class Configurator extends Component {
     render() {
         return (
             <div className="container">
-                <div className="row justify-content-between">
-                    <div className="col-4">
-                        One 
+                <div className="row">
+                    <div className="col-6">
+                        <h1 className="configurator-title">Save</h1> 
                     </div>
                     <div className="col-6">
                         Two
@@ -17,4 +21,18 @@ class Configurator extends Component {
     }
 };
 
-export default Configurator;
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({ getConfiguratorData }, dispatch);
+};
+
+const mapStateToProps = (state) => {
+    return {
+        title: state.configurator.title,
+        description: state.configurator.description
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Configurator);
